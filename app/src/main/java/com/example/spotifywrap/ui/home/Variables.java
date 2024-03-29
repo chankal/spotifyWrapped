7348e2c568010e59b4318b0105cbde342e141dce
Brisabrin <Brisabrin@gmail.com>
2024-03-29 10:43:16 -0400

    defined all endpoints, pending extract response

---
+++ app/src/main/java/com/example/spotifywrap/ui/home/Variables.java
@@ -0,0 +1,9 @@
+package com.example.spotifywrap.ui.home;
+
+public class Variables {
+    static final String CLIENT_ID = "65a980a3209b4051ad5e990fb4907c59";
+    static final String REDIRECT_URI = "com.spotifyapiexample://callback";
+    static final int REQUEST_CODE = 1337;
+    static final String SCOPES = "user-read-recently-played,user-library-modify,user-read-email,user-read-private";
+
+}
