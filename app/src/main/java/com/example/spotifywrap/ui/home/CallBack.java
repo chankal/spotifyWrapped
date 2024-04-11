7e54a37e3e33e9d2d2cb7d5296b04a30bbe75d4b
Brisabrin <Brisabrin@gmail.com>
2024-04-11 14:55:34 -0400

    added auth and signup

--- app/src/main/java/com/example/spotifywrap/ui/home/CallBack.java
+++
@@ -1,4 +0,0 @@
-package com.example.spotifywrap.ui.home;
-
-public class CallBack {
-}
--- app/src/main/java/com/example/spotifywrap/ui/home/User.java
+++ app/src/main/java/com/example/spotifywrap/ui/home/User.java
@@ -2,10 +2,8 @@ package com.example.spotifywrap.ui.home;
 
 
 public class User {
-
-    public String birthdate;
-    public String country;
     public String display_name;
     public String email;
     public String id;
+
 }
